dc1f108df311d3e64806bb190ec0205aaf0a0ca1
sakshigupta2 <57287093+sakshigupta2@users.noreply.github.com>
2020-01-05 15:04:58 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,30 @@
+
+var database;
+var gameState = 0;
+var playerCount;
+var allPlayers;
+var distance = 0;
+var form, player, game;
+
+function setup(){
+    database = firebase.database();
+    createCanvas(500,500);
+    game = new Game();
+    game.getState();
+    game.start();
+    
+}
+
+function draw(){
+    background("green");
+    if (playerCount===4){
+        game.update(1);
+
+    }
+    if (gameState===1){
+        clear();
+        game.play();
+    }
+    
+}
+
